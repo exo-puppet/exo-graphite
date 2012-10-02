bb40f826454fdc0d82f15632fa712739b1dadcba
Frederic DROUET <fdrouet@exoplatform.com>
2012-10-02 23:44:56 +0200

    SWF-2153 : remove trailing spaces

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,22 +2,22 @@
 #
 # This class manage the graphite parameters for different OS
 class graphite::params {
-	
+
 	case $::operatingsystem {
 		/(Ubuntu)/: {
-		    
+
 		    $services_scripts_dir = "/etc/init.d"
 		    $service_carbon_name  = "carbon-cache"
-		    
+
 		    $install_dir        = "/opt/graphite"
             $tempdir4install    = "/tmp/graphite"
-            
+
             $apache_conf_dir    = "${install_dir}/apache"
             $apache_docroot     = "${install_dir}/webapp"
             $apache_include_conf_file = "${$apache_conf_dir}/graphite.conf"
-            
+
             $packages           = [ "python-django", "python-django-tagging", "python-memcache", "python-amqplib", "python-twisted", "python-twisted-core", "python-twisted-web", "python-twisted-names", "python-simplejson", "python-ldap" ]
-            
+
             $graphite_download_base_url = "http://launchpad.net/graphite/0.9"
             $whisper_version            = "0.9.9"
             $carbon_version             = "0.9.9"
@@ -27,7 +27,7 @@ class graphite::params {
             # python version
             case $::lsbdistrelease {
                 /(10.04)/: {
-                    fail ("The ${module_name} puppet module is NOT SUPPORTED on $::operatingsystem $::operatingsystemrelease (and will never be)") 
+                    fail ("The ${module_name} puppet module is NOT SUPPORTED on $::operatingsystem $::operatingsystemrelease (and will never be)")
                 }
                 /(10.10)/: {
                     $python_version = "2.6"
